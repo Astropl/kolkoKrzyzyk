75c724d41f9e12d60208d83f2e384900bb0abbf6
Astropl <30636834+Astropl@users.noreply.github.com>
2017-12-21 22:16:55 +0100

    Add files via upload
    Dodałem opcję sprawdzania wygranych zarówno dla kołek i krzyzyków.
    Teraz pracuję nad wyborem 1. Gracz- Gracz. 2. Gracz - komputer.

--- Main.java
+++ Main.java
@@ -1,23 +1,56 @@
 import javafx.scene.control.Tab;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        String tab[][] = new String[3][3];
+        //String checkWin;
+        CheckWin checkWin = new CheckWin();
+        Boolean stanWygranej = false;
+        //String checkWin;
         Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
         PlanszaPierwsza planszaPierwsza = new PlanszaPierwsza();
         RysujPlansze rysujPlansze = new RysujPlansze();
         planszaPierwsza.planszaPierwsza();
-        for (int i = 0; i < 9; i++) {
-            System.out.println("\nPodaj pole i po enterze znak ( a1 ) x lub o");
+        System.out.println("Wybierz czy grasz z kolegą czy z komputerem.\n1. Z komputerem.\n2. Z kolegą");
+        int wybor = 0;
+        do {
+            wybor = sc.nextInt();
+            if (wybor == 1) {
+                System.out.println(" Grasz z komputerem. Twój pierwszy ruch.\nGrasz znakami X");
+            } else if (wybor == 2) {
+                System.out.println(" Grasz z kolegą Twój pierwszy ruch.\nGrasz znakami X, a kolega O");
+            } else {
+                System.out.println(" Wybrany zły numer. Wybierz ponownie");
+            }
+        } while ((wybor != 1) && (wybor != 2));
+
+        for (int i = 0; i < 100; i++) {// ZMIenic pozniej na petle while
+            System.out.println("\nPodaj pole np a1,a2,a3  b1,b2,b3  c1,c2,c3  i po enterze nacisnij znak x lub o :");
+
+// wybor gracza czy komputer zrobie za pomoca modulo.
+            
             String podajPole = sc.nextLine();
             String znak = sc.nextLine();
 
             //rysujPlansze.podajPole(podajPole);
             rysujPlansze.podajPole(podajPole, znak);
+            rysujPlansze.checkWin();
+            if (rysujPlansze.stanWygranej == true) {
+                System.out.println("Koniec.");
+                i = 100;
+            }
+            //checkWin.checkWin();
+
         }
 //rysujPlansze(podajPole,znak)
+
+        // zrobic obsługę wygranych :>
+
+
     }
 }
